dc4675bd500b57378d746151ba0a1885e24e2fb9
DuelistRag3 <christian.dullin@web.de>
2021-12-11 12:58:32 +0100

    Home and Installer Module

---
+++ Modules/Installer/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('installer::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('installer.name') !!}
+    </p>
+@endsection
---
+++ Modules/Installer/Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module Installer</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/installer.css') }}"> --}}
+
+    </head>
+    <body>
+        @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/installer.js') }}"></script> --}}
+    </body>
+</html>
