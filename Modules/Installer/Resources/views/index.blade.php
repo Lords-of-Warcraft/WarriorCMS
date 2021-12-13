f31dec88ab8b10e84dfcf3255f46cae9beff7957
DuelistRag3 <christian.dullin@web.de>
2021-12-13 18:57:40 +0100

    Update installer

--- Modules/Installer/Resources/views/index.blade.php
+++ Modules/Installer/Resources/views/index.blade.php
@@ -1,9 +1,60 @@
 @extends('installer::layouts.master')
-
+@section('darkmodeslider')
+<div class="slidercontainer">
+    <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
+</div>
+@endsection
 @section('content')
-    <h1>Hello World</h1>
+<div class="center">
+    <img src="/img/logo.png" width="200px">
+</div>
+<div class="center mt-5">
+    <i data-feather="caret-left" class="section-arrow"></i>
+    <div class="box" id="start">
+      <h2>Welcome</h2>
+      <hr>
+      <p>WarriorCMS is an open source content menagement system<br> build in Laravel by the World of Warriors Private Server Team.</p>
+    </div>
+    <i data-feather="caret-right" class="section-arrow"></i>
+</div>
+@endsection
 
-    <p>
-        This view is loaded from module: {!! config('installer.name') !!}
-    </p>
+@section('menu')
+<div class="menu">
+    <div class="navigation">
+        <ul>
+            <li class="list active">
+                <a href="#start">
+                    <span class="icon"><i data-feather="home"></i></span>
+                    <span class="text">Start</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#website">
+                    <span class="icon"><i data-feather="globe"></i></span>
+                    <span class="text">Website</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"><i data-feather="database"></i></span>
+                    <span class="text">Database</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"><i data-feather="server"></i></span>
+                    <span class="text">Realms</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"><i data-feather="user"></i></span>
+                    <span class="text">User</span>
+                </a>
+            </li>
+            <div class="indicator"></div>
+        </ul>
+    </div>
+</div>
 @endsection
--- Modules/Installer/Resources/views/layouts/master.blade.php
+++ Modules/Installer/Resources/views/layouts/master.blade.php
@@ -4,15 +4,31 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Installer</title>
+        <title>{{ env('APP_NAME') }} Installer</title>
+        <script src="https://cdn.jsdelivr.net/gh/steinhaug/feathericons-forkitup-edition@4.28.0-fu-edition/dist/feather.min.js"></script>
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/installer.css') }}"> --}}
+        {{-- Laravel Mix - CSS File --}}
+        <link rel="stylesheet" href="{{ mix('css/installer.css') }}"> 
 
     </head>
     <body>
+        @yield('darkmodeslider')
         @yield('content')
+        @yield('menu')
 
+        <script>
+            const list = document.querySelectorAll('.list');
+            function activeLink(){
+                list.forEach((item) => 
+                item.classList.remove('active'));
+                this.classList.add('active');
+            }
+            list.forEach((item) => 
+            item.addEventListener('click',activeLink));
+        </script>
+        <script>
+            feather.replace()
+        </script>
         {{-- Laravel Mix - JS File --}}
         {{-- <script src="{{ mix('js/installer.js') }}"></script> --}}
     </body>
