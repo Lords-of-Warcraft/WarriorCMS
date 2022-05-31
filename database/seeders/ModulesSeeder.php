02231fa878175e866934d4b1998988701778dcab
Christian Dullin <christian.dullin@web.de>
2022-05-31 21:52:13 +0200

    Create Modules and settings table

---
+++ database/seeders/ModulesSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ModulesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('modules')->insert([
+            [
+                'name'          => 'Admin',
+                'status'        => 1,
+                'icon'          => 'cog',
+                'has_settings'  => 0,
+            ],
+            [
+                'name'      => 'Installer',
+                'status'    => 1,
+                'icon'      => 'microship',
+                'has_settings'  => 0,
+            ],
+            [
+                'name'      => 'Home',
+                'status'    => 1,
+                'icon'      => 'home',
+                'has_settings'  => 1,
+            ],
+            [
+                'name'      => 'News',
+                'status'    => 1,
+                'icon'      => 'newspaper',
+                'has_settings'  => 1,
+
+            ],
+            [
+                'name'      => 'User',
+                'status'    => 1,
+                'icon'      => 'user',
+                'has_settings'  => 1,
+            ],
+        ]);
+    }
+}
